c359a2656c67227c156f8205ff1785d216a26f7f
HuyPC <sinhhuy.bui@gmail.com>
2019-02-18 08:42:50 +0700

    Task Management

---
+++ app/Console/Controller.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HuyPC
+ * Date: 2/15/2019
+ * Time: 3:11 PM
+ */
---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function create()
+    {
+
+    }
+    public function store()
+    {
+
+    }
+    public function show()
+    {
+
+    }
+    public function edit()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+    public function delete()
+    {
+
+    }
+
+}
+?>
